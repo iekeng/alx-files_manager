faed5a82c4afd476f90df7fd3e6766492f1cc4cd
Montanah <mititi.isaac@gmail.com>
2023-10-02 22:18:14 +0000

    Latest commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -15,4 +17,29 @@ router.get('/stats', (req, res) => {
 router.post('/users', (req, res) => {
   UsersController.postNew(req, res);
 });
-export default router;
+
+router.get('/connect', (req, res) => {
+  AuthController.getConnect(req, res);
+});
+
+router.get('/disconnect', (req, res) => {
+  AuthController.getDisconnect(req, res);
+});
+
+router.get('/users/me', (req, res) => {
+  UsersController.getMe(req, res);
+});
+
+router.post('/files', (req, res) => {
+  FilesController.postUpload(req, res);
+});
+
+router.get('/files/:id', (req, res) => {
+  FilesController.getShow(req, res);
+});
+
+router.get('/files', (req, res) => {
+  FilesController.getIndex(req, res);
+});
+
+module.exports = router;
