79793f6df1f32ee3cd5a42979fb8d7d6b9c37e7b
Montanah <mititi.isaac@gmail.com>
2023-10-04 08:15:47 +0000

    FilesController refactoring

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -43,11 +43,10 @@ class FilesController {
       return response.status(400).json({ error: missingFieldError });
     }
 
-    const files = dbClient.db.collection('files');
-
     const checkParentFolder = async () => {
       if (parentId) {
         const idObject = new ObjectID(parentId);
+        const files = dbClient.db.collection('files'); // Define 'files' here
         const file = await files.findOne({ _id: idObject, userId: user._id });
         if (!file) return 'Parent not found';
         if (file.type !== 'folder') return 'Parent is not a folder';
@@ -60,45 +59,20 @@ class FilesController {
       return response.status(400).json({ error: parentFolderError });
     }
 
-    if (type === 'folder') {
+    const insertFileToDatabase = async () => {
       try {
-        const result = await files.insertOne({
-          userId: user._id,
-          name,
-          type,
-          parentId: parentId || 0,
-          isPublic,
-        });
-        response.status(201).json({
-          id: result.insertedId,
-          userId: user._id,
-          name,
-          type,
-          isPublic,
-          parentId: parentId || 0,
-        });
-      } catch (error) {
-        console.log(error);
-        return response.status(500).json({ error: 'Internal server error' });
-      }
-    } else {
-      const filePath = process.env.FOLDER_PATH || '/tmp/files_manager';
-      const fileName = `${filePath}/${uuidv4()}`;
-      const buff = Buffer.from(data, 'base64');
+        const files = dbClient.db.collection('files'); // Define 'files' here
+        const filePath = process.env.FOLDER_PATH || '/tmp/files_manager';
+        const fileName = `${filePath}/${uuidv4()}`;
+        const buff = Buffer.from(data, 'base64');
 
-      try {
         try {
           await fs.mkdir(filePath);
         } catch (error) {
           // Pass. Error raised when the file already exists
         }
         await fs.writeFile(fileName, buff, 'utf-8');
-      } catch (error) {
-        console.log(error);
-        return response.status(500).json({ error: 'Internal server error' });
-      }
 
-      try {
         const result = await files.insertOne({
           userId: user._id,
           name,
@@ -127,6 +101,33 @@ class FilesController {
         console.log(error);
         return response.status(500).json({ error: 'Internal server error' });
       }
+      return null;
+    };
+
+    if (type === 'folder') {
+      try {
+        const files = dbClient.db.collection('files'); // Define 'files' here
+        const result = await files.insertOne({
+          userId: user._id,
+          name,
+          type,
+          parentId: parentId || 0,
+          isPublic,
+        });
+        response.status(201).json({
+          id: result.insertedId,
+          userId: user._id,
+          name,
+          type,
+          isPublic,
+          parentId: parentId || 0,
+        });
+      } catch (error) {
+        console.log(error);
+        return response.status(500).json({ error: 'Internal server error' });
+      }
+    } else {
+      await insertFileToDatabase();
     }
     return null;
   }
@@ -163,8 +164,9 @@ class FilesController {
     } else {
       query = { userId: user._id, parentId: ObjectID(parentId) };
     }
-    files.aggregate(
-      [
+
+    try {
+      const result = await files.aggregate([
         { $match: query },
         { $sort: { _id: -1 } },
         {
@@ -173,9 +175,9 @@ class FilesController {
             data: [{ $skip: 20 * parseInt(pageNum, 10) }, { $limit: 20 }],
           },
         },
-      ],
-    ).toArray((err, result) => {
-      if (result) {
+      ]).toArray();
+
+      if (result && result.length > 0) {
         const final = result[0].data.map((file) => {
           const tmpFile = {
             ...file,
@@ -185,13 +187,16 @@ class FilesController {
           delete tmpFile.localPath;
           return tmpFile;
         });
-        // console.log(final);
+
         return response.status(200).json(final);
       }
-      console.log('Error occured');
+
+      console.log('Error occurred');
       return response.status(404).json({ error: 'Not found' });
-    });
-    return null;
+    } catch (error) {
+      console.log(error);
+      return response.status(500).json({ error: 'Internal server error' });
+    }
   }
 
   static async putPublish(request, response) {
@@ -204,13 +209,18 @@ class FilesController {
     const idObject = new ObjectID(id);
     const newValue = { $set: { isPublic: true } };
     const options = { returnOriginal: false };
-    files.findOneAndUpdate({ _id: idObject, userId: user._id }, newValue, options, (err, file) => {
+
+    try {
+      const file = await files.findOneAndUpdate({ _id: idObject, userId: user._id },
+        newValue, options);
       if (!file.lastErrorObject.updatedExisting) {
         return response.status(404).json({ error: 'Not found' });
       }
       return response.status(200).json(file.value);
-    });
-    return null;
+    } catch (error) {
+      console.log(error);
+      return response.status(500).json({ error: 'Internal server error' });
+    }
   }
 
   static async putUnpublish(request, response) {
@@ -223,28 +233,38 @@ class FilesController {
     const idObject = new ObjectID(id);
     const newValue = { $set: { isPublic: false } };
     const options = { returnOriginal: false };
-    files.findOneAndUpdate({ _id: idObject, userId: user._id }, newValue, options, (err, file) => {
+
+    try {
+      const file = await files.findOneAndUpdate({ _id: idObject, userId: user._id },
+        newValue, options);
       if (!file.lastErrorObject.updatedExisting) {
         return response.status(404).json({ error: 'Not found' });
       }
       return response.status(200).json(file.value);
-    });
-    return null;
+    } catch (error) {
+      console.log(error);
+      return response.status(500).json({ error: 'Internal server error' });
+    }
   }
 
   static async getFile(request, response) {
     const { id } = request.params;
     const files = dbClient.db.collection('files');
     const idObject = new ObjectID(id);
-    files.findOne({ _id: idObject }, async (err, file) => {
+    const user = await FilesController.getUser(request);
+
+    try {
+      const file = await files.findOne({ _id: idObject });
+
       if (!file) {
         return response.status(404).json({ error: 'Not found' });
       }
 
       console.log(file.localPath);
+
       if (file.isPublic) {
         if (file.type === 'folder') {
-          return response.status(400).json({ error: 'No content on folder' });
+          return response.status(400).json({ error: 'No content in folder' });
         }
 
         try {
@@ -260,35 +280,31 @@ class FilesController {
           console.log(error);
           return response.status(404).json({ error: 'Not found' });
         }
+      } else if (file.userId.toString() === user._id.toString()) {
+        if (file.type === 'folder') {
+          return response.status(400).json({ error: 'No content in folder' });
+        }
+
+        try {
+          let fileName = file.localPath;
+          const size = request.param('size');
+          if (size) {
+            fileName = `${file.localPath}_${size}`;
+          }
+          const contentType = mime.contentType(file.name);
+          return response.header('Content-Type', contentType).status(200).sendFile(fileName);
+        } catch (error) {
+          console.log(error);
+          return response.status(404).json({ error: 'Not found' });
+        }
       } else {
-        const user = await FilesController.getUser(request);
-        if (!user) {
-          return response.status(404).json({ error: 'Not found' });
-        }
-
-        if (file.userId.toString() === user._id.toString()) {
-          if (file.type === 'folder') {
-            return response.status(400).json({ error: 'No content on folder' });
-          }
-
-          try {
-            let fileName = file.localPath;
-            const size = request.param('size');
-            if (size) {
-              fileName = `${file.localPath}_${size}`;
-            }
-            const contentType = mime.contentType(file.name);
-            return response.header('Content-Type', contentType).status(200).sendFile(fileName);
-          } catch (error) {
-            console.log(error);
-            return response.status(404).json({ error: 'Not found' });
-          }
-        } else {
-          console.log(`Wrong user: file.userId=${file.userId}; userId=${user._id}`);
-          return response.status(404).json({ error: 'Not found' });
-        }
+        console.log(`Wrong user: file.userId=${file.userId}; userId=${user._id}`);
+        return response.status(404).json({ error: 'Not found' });
       }
-    });
+    } catch (error) {
+      console.log(error);
+      return response.status(500).json({ error: 'Internal server error' });
+    }
   }
 }
 
